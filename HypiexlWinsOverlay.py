9906090ea7baaed64c4da25cf8d69a1cdc700346
Colon101 <99831740+Colon101@users.noreply.github.com>
2023-08-01 03:03:27 +0300

    and thats it for the night

--- HypiexlWinsOverlay.py
+++ HypiexlWinsOverlay.py
@@ -1,2 +1,20 @@
 from tkinter import *
-import HypixelWinsOverlay.GetData
+from GetData import GetBridgeWinsData
+from tkinter import messagebox
+class HypixelWinsOverlayGUI:
+    def __init__(self):
+        self.window = Tk()
+        l1 = Label(self.window,text="Please type your API key")
+        l1.pack()
+        self.e1 = Entry(self.window)
+        self.e1.pack()
+        b1 = Button(self.window,text="Submit API Key",command=self.run_api_key_submit)
+        b1.pack()
+        self.window.mainloop()
+    def run_api_key_submit(self):
+        apientry = self.e1.get()
+        self.GBWD = GetBridgeWinsData(apientry)
+        if self.GBWD.ValidApiToken == False:
+            messagebox.showerror("API Key is invalid","API Key is invalid")
+if __name__ == "__main__":
+    winoverlay = HypixelWinsOverlayGUI()
